d93ddbf3a5077637a983bb7800e2688adc556365
Ethan Price <eprice16@tsaremedia.com>
2025-03-02 23:20:25 -0500

    Add project files.

---
+++ RocketLeagueApp/MVVM/Model/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueApp.Model
+{
+
+
+    public class PlayerStats
+    {
+        public string? Rank { get; set; }
+        public double ShootingPercentage { get; set; }
+        public double ShotsPerGame { get; set; }
+        public double SavePercentage { get; set; }
+        public double SavesPerGame { get; set; }
+        public double GoalPerMatch { get; set; }
+    }
+
+}
---
+++ RocketLeagueApp/MVVM/ViewModel/StatsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using RocketLeagueApp.Model;
+
+
+namespace RocketLeagueApp.ViewModel
+
+{ 
+    class StatsViewModel
+    {
+        private readonly string apiKey = "5d1f8cb5-b2b0-4052-8619-f98e09c857cb";
+        private readonly string baseUrl = "https://api.tracker.gg/api/v2/rocket-league/standard/profile/";
+
+        public async Task<PlayerStats> GetPlayerStats(string platform, string playername)
+        {
+            string url = $"{baseUrl}{platform}/{playername}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Tracker API Key", apiKey);
+                HttpResponseMessage response = await client.GetAsync(url);
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+
+                JObject data = JObject.Parse(jsonResponse);
+                return new PlayerStats
+                {
+                    Rank = data["data"]["segments"][0]["stats"]["tier"]["metadata"]["name"].ToString(),
+                    ShootingPercentage = Convert.ToDouble(data["data"]["segments"][0]["stats"]["shootingPercentage"]["value"]),
+                    ShotsPerGame = Convert.ToDouble(data["data"]["segments"][0]["stats"]["shotsPerGame"]["value"]),
+                    SavePercentage = Convert.ToDouble(data["data"]["segments"][0]["stats"]["savesPercentage"]["value"]),
+                    SavesPerGame = Convert.ToDouble(data["data"]["segments"][0]["stats"]["savesPerGame"]["value"]),
+                    GoalPerMatch = Convert.ToDouble(data["data"]["segments"][0]["stats"]["goalsPerGame"]["value"])
+                };
+            }
+        }
+
+    }
+}
+
